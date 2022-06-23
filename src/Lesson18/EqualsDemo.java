eec1cf300d4b652f3071188baafa46795e01efb6
Skrit-ex <skritex0@gmail.com>
2022-06-23 15:49:47 +0300

    Initial commit

---
+++ src/Lesson18/EqualsDemo.java
@@ -0,0 +1,12 @@
+package Lesson18;
+
+public class EqualsDemo {
+    public static void main(String[] args) {
+        Equals equals1 = new Equals("Alex", 55, true);
+        Equals equals2 = new Equals("Sam", 48, false);
+        Equals equals3 = new Equals("Sam", 48, false);
+
+        System.out.println("equals1 = equals2 ?  ==> " + equals1.equals(equals2));
+        System.out.println("equals2 = equals3 ?  ==> " + equals3.equals(equals2));
+    }
+}
