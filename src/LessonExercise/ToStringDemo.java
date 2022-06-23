f652c5c4fc874038f7f7780a4f4c4f59ecf241fc
Skrit-ex <skritex0@gmail.com>
2022-06-23 16:00:14 +0300

    Exercise ToString

---
+++ src/LessonExercise/ToStringDemo.java
@@ -0,0 +1,8 @@
+package LessonExercise;
+
+public class ToStringDemo {
+    public static void main(String[] args) {
+        ToString person = new ToString(" Alex",  29);
+        System.out.println (person);
+    }
+}
